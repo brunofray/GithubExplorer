13a00eca42feabbcad88ab9cacf4371abd3ad4e0
Bruno Fray <brunofray@gmail.com>
2021-03-16 19:43:19 -0300

    Utilizando o useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
   name: 'unform',
@@ -9,6 +12,15 @@ const repository = {
 }
 
 export function RepositoryList() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/brunofray/repos')
+      .then(response => response.json())
+      .then(date => setRepositories(data))
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
